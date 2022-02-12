60e089324648afd850474d23853731a0738f7b04
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-02-12 13:23:14 +0200

    Generate the PayTabs’ log file.

--- config/logging.php
+++ config/logging.php
@@ -99,6 +99,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'PayTabs' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/paytabs.log'),
+            'level' => 'info',
+        ],
     ],
 
 ];
