5cba10920c1fa2b3fa50b68da91069a7b694d24a
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-02-14 11:10:44 +0200

    modife CookieController

--- app/Http/Controllers/CookieController.php
+++ app/Http/Controllers/CookieController.php
@@ -13,7 +13,13 @@ class CookieController extends Controller
     {
         $minutes = 1;
         $response =  new Response("hello farah");
+
+        /* in this option the cookie will still alife for onle one minute */
         $response->withCookie(cookie('name', 'farah', $minutes));
+
+        /* in this option the cookie will still alife forever */
+        //$response->withCookie(cookie()->forever('name', 'value'));
+
         return $response;
     }
     public function getCookie(Request $request)
