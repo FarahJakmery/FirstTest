70d855aee943db1509d42b7de56eb06e8ff4ad32
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-02-16 20:29:06 +0200

    Create a layout "app1"

---
+++ resources/views/layouts/app1.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>FirstTest @yield('title')</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    @yield('style')
+</head>
+
+<body>
+    @include('partials.hero')
+
+    @include('partials.messages')
+
+    <section class="section">
+        @yield('content')
+    </section>
+
+    @include('partials.footer')
+
+    @yield('script')
+</body>
+
+</html>
