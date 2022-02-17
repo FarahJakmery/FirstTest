86866249b6bc1b113204b9577995d39f42500b32
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-02-17 16:17:02 +0200

    make some changes in app1 "layouts"

--- resources/views/layouts/app1.blade.php
+++ resources/views/layouts/app1.blade.php
@@ -4,13 +4,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>FirstTest @yield('title')</title>
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    {{-- <link href="{{ asset('js/jquery-3.5.1.min.js') }}" rel="stylesheet"> --}}
+
     @yield('style')
 </head>
 
@@ -25,7 +30,7 @@
 
     @include('partials.footer')
 
-    @yield('script')
+    @yield('scripts')
 </body>
 
 </html>
