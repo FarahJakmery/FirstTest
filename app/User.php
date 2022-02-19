c6ffd7de2649a0b6b80aebf5a3382aa144472502
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-02-19 15:21:22 +0200

    -Add (2) new fields to User Model -Add to functions to User Model

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'created_at', 'updated_at', 'remember_token', 'email_verified_at', 'two_factor_code', 'two_factor_expires_at',
     ];
 
     /**
@@ -28,6 +28,13 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    protected $dates = [
+        'updated_at',
+        'created_at',
+        'email_verified_at',
+        'two_factor_expires_at',
+    ];
+
     /**
      * The attributes that should be cast to native types.
      *
@@ -36,4 +43,29 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    /**
+     * Generate 6 digits MFA code for the User
+     */
+    public function generateTwoFactorCode()
+    {
+        $this->timestamps = false; //Dont update the 'updated_at' field yet
+
+        $this->two_factor_code = rand(1000000, 9999999);
+        $this->two_factor_expires_at = now()->addMinutes(10);
+        $this->save();
+    }
+
+    /**
+     * Reset the MFA code generated earlier
+     */
+    public function resetTwoFactorCode()
+    {
+        $this->timestamps = false; //Dont update the 'updated_at' field yet
+
+        $this->two_factor_code = null;
+        $this->two_factor_expires_at = null;
+        $this->save();
+    }
 }
