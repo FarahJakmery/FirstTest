bff9067d5325d0d354e6139390c6f385d44b771d
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-02-20 08:05:23 +0200

    Add New Routes & make some changes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestControllerController;
 use App\Http\Controllers\TestRemembermeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\TwoFactorController;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -24,23 +25,40 @@ Route::get('/cookie/set', 'CookieController@setCookie');
 Route::get('/cookie/get', 'CookieController@getCookie');
 
 #Remember me functionality in Laravel
-Route::get('/user-register', 'TestRemembermeController@registerform')->name('user.register');
-Route::post('/post-registration', 'TestRemembermeController@postRegistration')->name('post.register');
+// Route::get('/user-register', 'TestRemembermeController@registerform')->name('user.register');
+// Route::post('/post-registration', 'TestRemembermeController@postRegistration')->name('post.register');
 
-Route::get('/user-login', 'TestRemembermeController@loginform')->name('user.login');
-Route::post('/check-login', 'TestRemembermeController@checklogin')->name('post.login');
+// Route::get('/user-login', 'TestRemembermeController@loginform')->name('user.login');
+// Route::post('/check-login', 'TestRemembermeController@checklogin')->name('post.login');
 
-Route::get('/user-dashboard', 'TestRemembermeController@dashboard')->name('user.dashboard');
-Route::get('logout', 'TestRemembermeController@logout')->name('logout');
+// Route::get('/user-dashboard', 'TestRemembermeController@dashboard')->name('user.dashboard');
+// Route::get('logout', 'TestRemembermeController@logout')->name('logout');
 
-/* Products Routs */
-Route::resource('/products', 'ProductController');
 
-/* Cart Routes */
-Route::get('cart', 'ProductController@cart')->name('cart');
-Route::get('add-to-cart/{id}', 'ProductController@addToCart')->name('add.to.cart');
-Route::patch('update-cart', 'ProductController@updateCart')->name('update.cart');
-Route::delete('remove-from-cart', 'ProductController@remove')->name('remove.from.cart');
+
+// Route::redirect('/', '/login');
+// Route::redirect('/home', '/admin');
+Auth::routes(['register' => false]);
+
+
+/* Verification via Email Routes */
+Route::get('verify/resend', 'Auth\TwoFactorController@resend')->name('verify.resend');
+Route::resource('verify', 'Auth\TwoFactorController')->only(['index', 'store']);
+
+
+Route::group(['middleware' => ['auth', 'twofactor']], function () {
+    /* Dashboard Route */
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    /* Products Routs */
+    Route::resource('/products', 'ProductController');
+
+    /* Cart Routes */
+    Route::get('cart', 'ProductController@cart')->name('cart');
+    Route::get('add-to-cart/{id}', 'ProductController@addToCart')->name('add.to.cart');
+    Route::patch('update-cart', 'ProductController@updateCart')->name('update.cart');
+    Route::delete('remove-from-cart', 'ProductController@remove')->name('remove.from.cart');
+});
 
 
 
@@ -48,6 +66,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
